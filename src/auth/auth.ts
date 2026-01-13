06aba0e9100f8e178cc8c5dd438c20bd5c3d1eb5
WayneCommand <wayne@waynecommand.com>
2026-01-13 23:20:53 +0800

    Enhance authentication by adding `CLIENT_KEY` support and updating worker configuration typings.

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -2,5 +2,25 @@ import {Context} from "hono";
 
 // 基于 bearer 的委托认证
 export async function bearer(token: string, c: Context): Promise<boolean> {
-    return token === 'cloudflare_ai'
+
+    return await isSecure(token, c.env.CLIENT_KEY)
+}
+
+// 我自己的验证服务
+async function isSecure(clientKey, secretKey: string): Promise<boolean> {
+    let payload = {"ak": clientKey, "sk": secretKey}
+
+    let token = Buffer.from(JSON.stringify(payload)).toString('base64');
+
+    let response = await fetch("https://secure.waynecommand.com/bearer/verify", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + token
+        }
+    });
+
+    let result = await response.json() as {success: boolean}
+
+    return result.success
 }
